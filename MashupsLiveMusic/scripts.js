cf2200dbfb21750ea3d8113b172c363bed774631
Aaron Arntz <aarntz@gmail.com>
2014-03-01 20:57:17 -0500

    removed buttons, centered styling

--- MashupsLiveMusic/scripts.js
+++ MashupsLiveMusic/scripts.js
@@ -3,7 +3,9 @@
 //use venue Id to search for Events--finished 
 //Find today's date--finished
 
+//TO DO:
 
+//1. Match artist name exactly in Spotify or remove from list
 
 //check out SOLD OUT problem
 
@@ -28,29 +30,53 @@
 //spotify:user:aaronarntz:playlist:3w4eVv4ULYCkNyDjwYkVh7 
 //make playlist from artist events
 
-//Implement the JamBase, SoundCloud,  and Bandcamp options
+//Implement the JamBase, SoundCloud, and Bandcamp options, perhaps other databases as well
 
 //look at a different way to find events
 
+	var venueIdList = [];
+	var venueEventList = [];
+	var filteredDateEvent = []; 
+	var venueEventListArtists = [];
+	var artistNameArray = [];  
+	var spotifyArtistObjects = []; 
+	var spotifyArtistHREFs = []; 
+	var spotifyAlbumObjects =[]; 
+	var randomAlbums = []; 
+	var ajaxCounter = 0;
+	var today = Date.create().format(Date.ISO8601_DATE);
+
+	console.log(today);
 
 
-var venueIdList = [];
-var venueEventList = [];
-var filteredDateEvent = []; 
-var venueEventListArtists = [];
-var artistNameArray = [];  
-var spotifyArtistObjects = []; 
-var spotifyArtistHREFs = []; 
-var spotifyAlbumObjects =[]; 
-var randomAlbums = []; 
 
-var today = Date.create().format(Date.ISO8601_DATE);
+function initializeArrays(){
 
-console.log(today);
+	venueIdList = [];
+	venueEventList = [];
+	filteredDateEvent = []; 
+	venueEventListArtists = [];
+	artistNameArray = [];  
+	spotifyArtistObjects = []; 
+	spotifyArtistHREFs = []; 
+	spotifyAlbumObjects =[]; 
+	randomAlbums = []; 
+	ajaxCounter= 0; 
+	today = Date.create().format(Date.ISO8601_DATE);
+	console.log(today);
+	console.log("arrays initialized");
+}
+
+function clearCSS(){
+
+	$(".artist").remove();
+	$("iframe").remove();
+
+}
+
 
 
 function getBandsInTownVenueId(searchTerm) {
-
 	var bandsInTownURL = "http://api.bandsintown.com/venues/search.json?query="; 
 	var venueSearchTerm = searchTerm;
 	var bandsInTownKey = "&location=use_geoip&app_id=theyGood&callback=displayVenues";
@@ -67,13 +93,16 @@ function getBandsInTownVenueId(searchTerm) {
 
 			success: function (data) {
 				console.log("Venue ID success");
-				//console.log(data);			
-			for (var i = 0; i < data.length; i++) {
-			    getBandsInTownVenueEvents(data[i].id);
-				} 
+				//console.log(data);
+
+				for (var i = 0; i < data.length; i++) {
+					getBandsInTownVenueEvents(data[i].id);
+					}		
 			}
-		});
+		});	
 }
+
+
 
 function getBandsInTownVenueEvents(venueIdArray){ 
     
@@ -89,22 +118,22 @@ function getBandsInTownVenueEvents(venueIdArray){
 			},	
 
 			success: function (data) {	
-			    console.log("Venue Events success");
-			    //console.log(data);	
+				console.log("Venue Events success");
+				//console.log(data);	
+				
 				for (var i = 0; i < data.length; i++) {
-					  venueEventList.push(data[i]); 
-				 }; 
+					venueEventList.push(data[i]); 
+					}
 				 
-				 filterDate(); 
-				 getArtistNames();
-			 }	
+				filterDate(); 
+				getArtistNames();
+			}	
 		});		 
-		
 }
 
 
-
-function filterDate() {
+function filterDate(){
+	
 	console.log(venueEventList);
 	
 	for (var i = 0; i < venueEventList.length; i++) {
@@ -112,10 +141,10 @@ function filterDate() {
 		var eventTruncDate = venueEventList[i].datetime.truncate(10, 'right', '' );
 		
 		if (eventTruncDate === today){
-			 filteredDateEvent.push(venueEventList[i]);
-			 console.log("Show Today Success");
+			filteredDateEvent.push(venueEventList[i]);
+			console.log("There is a show today!");
 			}
-	};
+	}
 }
 
 
@@ -124,46 +153,21 @@ function getArtistNames(){
 	for (var i = 0; i < filteredDateEvent.length; i++) {
 		for (var j = 0; j < filteredDateEvent[i].artists.length; j++) {
 			artistNameArray.push(filteredDateEvent[i].artists[j].name);
-			//console.log(filteredDateEvent[i].artists[j].name);
 			}	
 		}	
 		artistNameArray = removeDuplicates(artistNameArray);
-		//console.log(artistNameArray); 
+		console.log(artistNameArray);
+		
 }
 	
-function removeDuplicates(_array) {
-		return _.uniq(_array);				
-	}	
-
 
 function getSpotifyArtistIDs(artistArray) {
-	console.log(artistArray); 
+	//console.log(artistArray); 
 	
 	for (var i = 0; i < artistArray.length; i++) {
 		requestSpotifyIDs(artistArray[i]);
-		$('#artistNames').append('<p>' + artistArray[i] + '</p'); 
+		$('#artistNames').append('<p class="artist">' + artistArray[i] + '</p'); 
 	}
-}
-
-function getSpotifyAlbums(hrefs){
-
-	for (var i = 0; i < hrefs.length; i++) {
-		requestSpotifyAlbums(hrefs[i]);
-	}
-}
-
-
-function aggSpotifyArtistIds() {
-
-	for (var i = 0; i < spotifyArtistObjects.length; i++) {
-		if (spotifyArtistObjects[i].artists.length){
-			for (var j = 0; j < spotifyArtistObjects[i].artists.length; j++) {
-				spotifyArtistHREFs.push(spotifyArtistObjects[i].artists[j].href);
-				}
-		}
-	}
-	spotifyArtistHREFs = removeDuplicates(spotifyArtistHREFs);
-	console.log(spotifyArtistHREFs);
 }
 
 function requestSpotifyIDs (artistName) {
@@ -180,16 +184,26 @@ function requestSpotifyIDs (artistName) {
 			},	
 
 			success: function (data) {	
-				//console.log(data);
-				console.log("success");	 
-				spotifyArtistObjects.push(data);
-				aggSpotifyArtistIds(); 
+				console.log("success");
+				console.log(data);	 
+				
+				for (var i = 0; i < data.artists.length; i++) {
+				    if (data.artists[i].name === artistName){
+						spotifyArtistHREFs.push(data.artists[i].href);
+				    }
+				}	
+		  	spotifyArtistHREFs = removeDuplicates(spotifyArtistHREFs);
+		  	console.log(spotifyArtistHREFs);	
 			}
 		});
 }
 
+function getSpotifyAlbums(hrefs){
 
-
+	for (var i = 0; i < hrefs.length; i++) {
+		requestSpotifyAlbums(hrefs[i]);
+	}
+}
 
 function requestSpotifyAlbums(hrefs) {
 	
@@ -232,7 +246,7 @@ function putPlaylistsOnPage(spotifyRandomAlbums){
 
 	for (var i = 0; i < spotifyRandomAlbums.length; i++) {
 		
-		var tempString = '<iframe src="https://embed.spotify.com/?uri=' + spotifyRandomAlbums[i] + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+		var tempString = '<iframe src="https://embed.spotify.com/?uri=' + spotifyRandomAlbums[i] + '" class=spotifyFrames" frameborder="0" allowtransparency="true"></iframe>';
 		console.log(tempString);
 
 		$('#playlistBox').append(tempString);
@@ -240,32 +254,43 @@ function putPlaylistsOnPage(spotifyRandomAlbums){
 }
 
 
+function removeDuplicates(_array) {
+		return _.uniq(_array);				
+	}	
 
 $(document).ready(function(){
 
-	$('#loadArtistsButton').click(function() {
-		
-		//maybe check for blank input box
-		var inputBoxText = $('#searchBox').val();
-		getBandsInTownVenueId(inputBoxText);	
-		
+	$('#searchBox').keypress(function(e){
+		// e.preventDefault();
+		if(e.keyCode == 13){ 
+			initializeArrays();
+			clearCSS();
+			var inputBoxText = $('#searchBox').val();
+			getBandsInTownVenueId(inputBoxText);		
+		}
 	});
-
-	$('#generateSpotifyId').click(function() {
-		getSpotifyArtistIDs(artistNameArray);
-		
-	});
-
-	$('#generateSpotifyAlbums').click(function(){
-		getSpotifyAlbums(spotifyArtistHREFs);
-
-	});
-
-	$('#generateSpotifyTracks').click(function(){
-		getRandomAlbum(spotifyAlbumObjects);
-
-	});
-
 });
+
+
+$(document).ajaxStop(function(){
+	console.log("ajax finished " + ajaxCounter); 
+	
+	if (ajaxCounter == 0){
+		getSpotifyArtistIDs(artistNameArray);
+	}
+
+	else if (ajaxCounter == 1){
+		getSpotifyAlbums(spotifyArtistHREFs);
+	}
+
+	else if (ajaxCounter == 2){
+		getRandomAlbum(spotifyAlbumObjects);
+	}
+
+	ajaxCounter++;
+});
+
+
+
 
 	
